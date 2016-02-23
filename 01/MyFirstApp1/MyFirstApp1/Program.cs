55f53eb0b4f4e9daf8ccfca19061b77bd2d0baee
tester <test@test.com>
2016-02-23 14:49:47 +0800

    第一个C#程序，hello world

---
+++ 01/MyFirstApp1/MyFirstApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//命名空间
+namespace MyFirstApp1
+{
+    //类
+    class Program
+    {
+
+        //Main方法，程序入口
+        static void Main(string[] args) 
+        {
+            Console.Write("hello world");   //命令，以分号结束
+                                            //调试：f5
+                                            //不调试，直接运行：ctrl +f5
+                                            //类是c#的最小组成单元
+                                            //命名空间是用来组织和管理类
+                                            //在命名空间MyFirstApp1 中包含了program的类，也可以包含其他类
+        }
+    }
+}
