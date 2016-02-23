1ee8b92a12ae8f76114c69c684316f7ce90eaf13
liuxingwang <lookphp@gmail.com>
2016-02-23 14:55:49 +0800

    hello world --the first app

--- 01/MyFirstApp1/MyFirstApp1/Program.cs
+++ 01/MyFirstApp1/MyFirstApp1/Program.cs
@@ -11,15 +11,16 @@ namespace MyFirstApp1
     class Program
     {
 
-        //Main方法，程序入口
+        //1、Main方法，程序入口
         static void Main(string[] args) 
         {
+            //3、输出语句
             Console.Write("hello world");   //命令，以分号结束
                                             //调试：f5
                                             //不调试，直接运行：ctrl +f5
                                             //类是c#的最小组成单元
                                             //命名空间是用来组织和管理类
-                                            //在命名空间MyFirstApp1 中包含了program的类，也可以包含其他类
+                                            //2、在命名空间MyFirstApp1 中包含了program的类，也可以包含其他类
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# c-learning
+## c-learning
 learn c#
+first class content
+new a project step:<br/>
+1、选择文件类型：文件->新建项目->模板->Visual c# -> 控制台应用程序（用于创建命令行应用程序的项目）<br/>
+2、填写文件名称和文件存储位置：名称->myfirstapp 位置：D:\c#\c-learning\01\<br/>
+3、c#源文件是以 .cs  结尾的
+
+##认识c#程序结构
+需要记住的4点：
+1、Main方法，是程序的入口<br/>
+2、在命名空间中包含了若干类，命名空间是用来组织和管理类的<br/>
+3、c#是由类组成的，类是c#的最小组成单元，声明类的关键字是class<br/>
+4、输出语句：Console.Write();<br/>
